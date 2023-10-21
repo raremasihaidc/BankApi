140cd0f875be7f8f6251a85a510bf9c5c53d3530
raremasihaidc <142901818+raremasihaidc@users.noreply.github.com>
2023-10-21 23:49:33 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using BankAPI.Data;
+using BankAPI.Handler;
+using BankAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<IUserRepo,UserRepo>();
+builder.Services.AddScoped<IUserHandler, UserHandler>();
+builder.Services.AddScoped<IWalletHandler, WalletHandler>();
+builder.Services.AddScoped<IWalletRepo, WalletRepo>();
+builder.Services.AddScoped<ITransactionHandler, TransactionHandler>();
+builder.Services.AddScoped<ITransactionRepo, TransactionRepo>();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BankDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"), sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure();
+        options.EnableSensitiveDataLogging();
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
